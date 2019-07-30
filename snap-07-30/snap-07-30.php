0abbc8778cfd21d3c98b66523e14b3a3547f3acd
Erica Tollefson <etollefson@cnm.edu>
2019-07-30 13:35:05 -0600

    Commit snap 07-30 updated with in-class answers;

--- snap-07-30/snap-07-30.php
+++ snap-07-30/snap-07-30.php
@@ -9,24 +9,32 @@
 /*** gets Tweets by Tweet Date
 *
 * @param \PDO $pdo PDO connection object
-* @param datetime $tweetDateTime tweet date to search for
+* @param datetime $tweetDate tweet date to search for
 * @return \SplFixedArray SplFixedArray of Tweets found or null if not found
 * @throws \PDOException when mySQL related errors occur
 * @throws \TypeError when variables are not the correct data type
 **/
 
-	public static function getTweetsByTweetDate(\PDO $pdo) : \SPLFixedArray {
-		// create query template
-		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetDate = :tweetDate";
-		$statement = $pdo->prepare($query);
-		$statement->execute();
+	public static function getTweetsByTweetDate(\PDO $pdo, DateTime $tweetDate) : \SPLFixedArray {
 
-		// bind the tweet content to the place holder in the template
-		$tweetDate = "%$tweetDate%";
-		$parameters = ["tweetDate" => $tweetDate];
+		$startDateString = $tweetDate->format('Y-m-d) . ' 00:00:00';
+		$startDate = new DateTime($startDateString);
+		$endDate = new DateTime($startDateString);
+		$endDate->add(new DateInterval('P1D'));
+
+		// create query template
+		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetDate >= :startDate AND tweetDate < :endDate";
+		$statement = $pdo->prepare($query);
+
+
+		//Bind the beginning and end dates to the place holder in the template
+		$parameters = [
+		'startDate' => $startDate->format("Y-m-d H:i:s.u"),
+		'endDate' => $endDate->format("Y-m-d H:i:s.u"),
+		];
 		$statement->execute($parameters);
 
-		// build an array of tweets
+		// build an array of tweets from the returned rows.
 		$tweets = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		while(($row = $statement->fetch()) !== false) {
