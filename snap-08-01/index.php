0af48e0bb99afae4ec3503d7a68e21a934bab33c
Erica Tollefson <etollefson@cnm.edu>
2019-08-01 08:17:01 -0600

    Commit snap 08-01 commit index.php and styles.css with answers to part 1;

---
+++ snap-08-01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- custom CSS stylesheet -->
+		<link rel="stylesheet" href="styles.css" type="text/css" />
+
+		<title>Snap Challenge 08-01</title>
+	</head>
+	<body>
+		<ul class="my-list">
+			<li class="my-item">1</li>
+			<li class="my-item">2</li>
+			<li class="my-item">3</li>
+			<li class="my-item">4</li>
+			<li class="my-item">5</li>
+			<li class="my-item">6</li>
+		</ul>
+
+
+
+
+	</body>
+</html>
