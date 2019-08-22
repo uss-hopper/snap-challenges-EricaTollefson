ba1c3dd0ab6923dffac598f60bfb2a1074b972d6
Erica Tollefson <etollefson@cnm.edu>
2019-08-22 09:26:17 -0600

    commit with in-class answer

--- snap-08-22/script.js
+++ snap-08-22/script.js
@@ -8,16 +8,25 @@ console.log(array);
 // or:
 // let reducer = (accumulator, currentValue) => accumulator + currentValue;
 //console.log(array.reduce(reducer));
+//in-class answer:
+let myArray = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811];
 
-
+let sum = myArray.reduce(function(accumulator, currentValue) {
+	return accumulator + currentValue;
+});
+console.log(sum);
 
 // Part 2, my answer:
-
-
-
 let array2 = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
 
 let reducer = (accumulator, currentValue) => accumulator + currentValue;
 let array3 = array2.reduce(reducer).split(' , ');
 let words = array3.join(" ");
 console.log(words);
+
+//in-class answer:
+
+const wordArray = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
+
+const myReducer = (accumulator, currentValue) => accumulator + " " + currentValue;
+console.log(wordArray.reduce(myReducer));
