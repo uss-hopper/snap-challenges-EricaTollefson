7f56314a0e6cb532224a37bccd04093b4ad476e3
Erica Tollefson <etollefson@cnm.edu>
2019-08-27 08:54:18 -0600

    commiting with my answers to snap challenge for 8-27

---
+++ snap-08-27/script.js
@@ -0,0 +1,38 @@
+/*part 1, example of looping through an array to count, then my answer:
+const arr = [1, 1, 2, 3, 3, 3];
+const count = {};
+
+for (let i = 0; i < arr.length; ++i) {
+
+	if (!count[arr[i]]){
+		count[arr[i]] = 0;
+	}
+
+	++count[arr[i]];
+	console.log(count)
+} */
+
+let arr = [set of numbers];
+let count = {} create set-up for answer
+
+for ( i=0, i - count array length, i increment) {
+	loop through the array
+	check for each number in the array, count arr.i
+	increment count of each number ++
+	console.log(count results);
+
+}
+
+
+
+
+//part 2, my answer:
+
+const numbers = numbers between 5 and 10 inclusive
+
+for numbers {
+	if (number % 2 = 0), return fizz,
+	 if else (number % 3 = 0), return buzz,
+		else (number % 2 = 0 && number % 3 == 0), return buzz,
+};
+console.log(numbers);
