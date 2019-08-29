e223efcadc5b4a26d9320a0d104c5c188736eee9
Erica Tollefson <etollefson@cnm.edu>
2019-08-29 08:57:18 -0600

    commit my answers to 8-29 snap challenge

---
+++ snap-08-29/script.js
@@ -0,0 +1,37 @@
+// part 1, my answer:
+
+let str = "Happy Monday";
+
+function (noVowels) {
+	for str remove vowels;
+	return string with no vowels;
+}
+
+print string;
+
+
+// part 2, my answer:
+
+let arr = [lots of numbers];
+
+function (last number) {
+	order the array small - large
+	return arr.length -1
+}
+
+print greatest number
+
+
+//part 3, my answer:
+
+let fib = [0, 1]
+
+function (fib) {
+	while length of items in arr < 100
+		add two items = 3rd item
+		let arr length increment,
+
+	return arr[];
+}
+
+print arr of 100 numbers
